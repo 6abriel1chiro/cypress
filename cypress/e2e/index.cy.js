c5d6784048bd9b2cff37ae7604ac14ad81ee9c1e
Angy Encinas <angy.encinas@ucb.edu.bo>
2022-11-17 10:10:58 -0400

    Actualizar pagina principal a version final

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -8,14 +8,10 @@ describe("Ver pagina principal", () => {
         cy.get("#boton-docente").should("contain","Soy docente");
         cy.get("#boton-estudiante").should("contain","Soy estudiante");
     });
-    it("el boton de docentes debe redireccionar a la pagina de docentes", () => {
+    it("el boton de Ingresar debe redireccionar a la pagina de docentes", () => {
         cy.visit('index.html');
         cy.get("#boton-docente").click();
         cy.url().should("include", "/htmls/docentes");
     });
-    it("el boton de estudiantes debe redireccionar a la pagina de estudiantes", () => {
-        cy.visit('index.html');
-        cy.get("#boton-estudiante").click();
-        cy.url().should("include", "/htmls/estudiantes");
-    });
+
   });
