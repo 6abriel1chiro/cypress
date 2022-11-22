00d4341ca11c5b4ddb545ead3238644a430788ab
Angy Encinas <angy.encinas@ucb.edu.bo>
2022-11-22 09:28:14 -0400

    crear enlace a cabecera

--- cypress/e2e/docentes.cy.js
+++ cypress/e2e/docentes.cy.js
@@ -1,6 +1,6 @@
 describe("Cambiar vista de usuario", () => {
   it("deberia ingresar a la pagina de Docentes", () => {
-    cy.visit('http://localhost:1234/htmls/docentes.html');
+    cy.visit('docentes.html');
     cy.get('#subject').select('Mate Basica:M. Soruco').should('have.value', 'Mate Basica')  });
 
 });
